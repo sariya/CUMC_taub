fd4e7ed2582a07bf56c9c257c2cfdad4895bc87b
Sanjeev Sariya <sanjeevsariya@gmail.com>
2018-11-07 18:23:48 -0500

    Calculating exon to gene ratio

--- microarray_parkinsons_project/exon_splicing_expression/calculate_splicing_perexon_withingene_withinsample.R
+++ microarray_parkinsons_project/exon_splicing_expression/calculate_splicing_perexon_withingene_withinsample.R
@@ -6,7 +6,10 @@
 #PD Karen Marder, Giuseppe Tosto
 #
 
+library(dplyr)
+library(purrr)
 library(data.table)
+
 transcripts<-fread("/mnt/mfs/hgrcgrid/shared/GT_ADMIX/PDexpression/HuEx-1_0-st-v2.na36.hg19.transcript.csv",header=TRUE,sep=",",skip=23)
 transcripts=as.data.frame(transcripts)
 print(dim(transcripts))
@@ -19,14 +22,34 @@ probesets<-probesets[which(probesets$crosshyb_type==1),]
 print(dim(probesets)) #1135900
 
 expression_gene<-fread("/mnt/mfs/hgrcgrid/shared/GT_ADMIX/PDexpression/sanjeev_analyses/PD_RMAnormalized_core.txt",header=TRUE)
-expression_exon<-fread("/mnt/mfs/hgrcgrid/shared/GT_ADMIX/PDexpression/sanjeev_analyses/rscripts/PD_RMAnormalized_probe.txt",header=TRUE)
+expression_exon<-fread("/mnt/mfs/hgrcgrid/shared/GT_ADMIX/PDexpression/sanjeev_analyses/rscripts/PD_RMAnormalized_exon_filtered.txt",header=TRUE)
 
 expression_gene<-as.data.frame(expression_gene)
 expression_exon<-as.data.frame(expression_exon)
 
 print(dim(expression_gene)) #22011
-print(dim(expression_exon)) #1411399
+print(dim(expression_exon)) #516081
 
+###########################################################################
+#Fix CEL ids from filtered exon expression data. replace - with .
+#https://stackoverflow.com/questions/39997273/r-combine-several-gsub-function-ina-pipe
+#####################################################################################################
+
+temp_exoncolnames<-colnames(expression_exon)
+
+for(i in 1:length(temp_exoncolnames)){
+ifelse((grep("-",temp_exoncolnames[i]) ),temp_exoncolnames[i]<-gsub("-",".",temp_exoncolnames[i]),temp_exoncolnames[i]<-temp_exoncolnames[i] )
+}
+
+#
+#Filtered Probes don't have X in them. Fix it.
+#
+
+for(i in 1:length(temp_exoncolnames)){
+ifelse((grep("CEL",temp_exoncolnames[i]) ),temp_exoncolnames[i]<-paste("X",temp_exoncolnames[i],sep=""),temp_exoncolnames[i]<-temp_exoncolnames[i] )
+}
+
+colnames(expression_exon)<-temp_exoncolnames
 print(length(intersect(expression_gene$V1,transcripts$transcript_cluster_id))) #22011
 
 ###############################################################
@@ -47,11 +70,11 @@ print(dim(filtered.gene_expression))
 
 #exon probeset means normalized at probeset using RMA
 keep_exonprobesets<-intersect(expression_exon$V1,probesets$probeset_id)
-print(length(keep_exonprobesets)) #1135900
+print(length(keep_exonprobesets)) #379374
 
 #Keep exon probeset whose annotation information is found in probsethg19 na30
 expression_exon.annot<-expression_exon[match(keep_exonprobesets,expression_exon$V1),]
-print(dim(expression_exon.annot)) #1135900
+print(dim(expression_exon.annot)) #379374
 
 ########################################################################
 #Filter probeset annotation for filtered genes only
@@ -61,63 +84,120 @@ print(length(intersect(filtered.gene_expression$V1,probesets$transcript_cluster_
 #annotation probesets
 probesets_transcript_found.geneexpression<-probesets[ which(probesets$transcript_cluster_id %in% gene_to_keep_found_probesetannot.transcript),]
 
+print(dim(probesets_transcript_found.geneexpression)) ##582790     39
+
 #annotation probesets
-print(dim(probesets_transcript_found.geneexpression)) #582790
-length(unique(probesets_transcript_found.geneexpression$transcript_cluster_id))#17873
 
+print(length(unique(probesets_transcript_found.geneexpression$transcript_cluster_id)))#17873
+
+##############################
 #--exon probesets
-print(length(intersect(expression_exon.annot$V1,probesets_transcript_found.geneexpression$probeset_id))) #582790
+##########################
+#--find the ones for which annotation is present
 
-exonprobesets_genesfound<-intersect(expression_exon.annot$V1,probesets_transcript_found.geneexpression$probeset_id) #582790
+print(length(intersect(expression_exon.annot$V1,probesets_transcript_found.geneexpression$probeset_id))) #254798
+exonprobesets_genesfound<-intersect(expression_exon.annot$V1,probesets_transcript_found.geneexpression$probeset_id) #
 
 exon_probsets_genefounds<-expression_exon.annot[match(exonprobesets_genesfound,expression_exon.annot$V1),]
-print(dim(exon_probsets_genefounds)) #582790
+print(dim(exon_probsets_genefounds)) #254798
+
+##
+#Filter probeset annotation based on exon probsets availalbe
+##
+
+print(length(intersect(exon_probsets_genefounds$V1,probesets_transcript_found.geneexpression$probeset_id))) #254798
+probsets_filtered.annotation_foundexonexpression<-intersect(exon_probsets_genefounds$V1,probesets_transcript_found.geneexpression$probeset_id)
+print(length(probsets_filtered.annotation_foundexonexpression))
+
+filtered_annotation_foundexonexpression<-probesets_transcript_found.geneexpression[match(probsets_filtered.annotation_foundexonexpression,probesets_transcript_found.geneexpression$probeset_id),]
+print(dim(filtered_annotation_foundexonexpression))
+
+genes_further_filtered<-intersect(filtered.gene_expression[,1],filtered_annotation_foundexonexpression$transcript_cluster_id)
+filtered.gene_expression2<-filtered.gene_expression[match(genes_further_filtered,filtered.gene_expression[,1]),]
+#
+#Update colnmae of gene expressions to merge and later get exon ratio
+#
+
+newcolnames_geneexpression<-c("geneExpression_transcriptID", colnames(filtered.gene_expression2)[grep("CEL",colnames(filtered.gene_expression2))])
+colnames(filtered.gene_expression2)<-newcolnames_geneexpression # 16941   225
 
 #
 #For filtered genes and exons now get per exon expression
 #
 
-exon_splicing_index<- as.data.frame(matrix(nrow=nrow(exon_probsets_genefounds), ncol=224))
+###https://stackoverflow.com/questions/28765268/how-to-do-iterative-joins-in-plyr##
+
+join_exon.geneExprs.probeids_transcriptids<-(left_join(exon_probsets_genefounds,filtered_annotation_foundexonexpression,by=c("V1"="probeset_id")) %>% left_join(.,filtered.gene_expression2,by=c("transcript_cluster_id"="geneExpression_transcriptID")))
+
+print(length(grep("CEL",colnames(join_exon.geneExprs.probeids_transcriptids))))
+
+##https://stackoverflow.com/questions/41452097/match-by-id-and-divide-column-values-across-two-dataframes ##
+
+exonexp.ratio_with_geneexpres<-cbind(id=join_exon.geneExprs.probeids_transcriptids[,1], map2_df(join_exon.geneExprs.probeids_transcriptids[,2:225], join_exon.geneExprs.probeids_transcriptids[,264:487], `/`))
 
 #
-#Run loop for each exon filtered 
-#Find index of the exon probeset in annotated data 
-#Then find transcript of the exon. Find gene expression of the transcript of exon probeset 
-#get ratio of exon to the gene expression
+#For filtered genes and exons now get per exon expression
 #
-for(i in 1:nrow(exon_probsets_genefounds)) {
 
-#--get index of probeset from annotation file
-index_probeset<- which(probesets_transcript_found.geneexpression$probeset_id== exon_probsets_genefounds[i,1])
+print(dim(exonexp.ratio_with_geneexpres))  ##254798
+print("we have calculated ratio for SI")
 
-if(length(index_probeset) !=0 ){
+#
+#Print spliced exon ratio to files
+#make one chunk of 25000 exon
+#
 
-#--get transcript id of the index found probeset id
-transcriptid<-probesets_transcript_found.geneexpression[index_probeset,"transcript_cluster_id"]
+removed.x_colnames<-colnames(exonexp.ratio_with_geneexpres) %>% gsub(".x","",.)
+colnames(exonexp.ratio_with_geneexpres)<-removed.x_colnames
 
-#find index of the transcript id found in gene expression data
-index_transcript_matched_gene.expression<-which(filtered.gene_expression$V1==transcriptid)
+list_split_indexExon<-split(exonexp.ratio_with_geneexpres, (seq(nrow(exonexp.ratio_with_geneexpres))-1) %/% 25000) 
+length(list_split_indexExon) # 11 #
 
-if(length(index_transcript_matched_gene.expression)!=0){
-
-#--calculate ratio of the exon with respect to the gene within sample
-exon_splicing_index[i,1:224]<-exon_probsets_genefounds[i,2:225]/filtered.gene_expression[index_transcript_matched_gene.expression,2:225]
-}
-else{
-exon_splicing_index[i,1:224]<-NA
-}
-
-}
-else{
-exon_splicing_index[i,1:224]<-NA
+for(i in 1:length(list_split_indexExon) )
+{
+file<-paste("exon_chunk_ratio",i,sep="_") #--make file name for chunk
+file_dest<-paste("./ratio_exon_to_gene",file,sep="/")
+write.table(list_split_indexExon[[i]], file_dest, sep="\t", quote=F, row.names=FALSE,col.names=TRUE)
 }
 
 
-}
-#--for loop ends
-print(dim(exon_splicing_index)) 
-
-row.names(exon_splicing_index)<-exon_probsets_genefounds[,1]
-cel_names<-grep("CEL",colnames(joined_annot_exon_filtered),value=TRUE)
 
 
+"R version 3.4.2 (2017-09-28)
+Platform: x86_64-pc-linux-gnu (64-bit)
+Running under: Debian GNU/Linux 9 (stretch)
+
+Matrix products: default
+BLAS: /mnt/mfs/cluster/bin/R-3.4/lib/libRblas.so
+LAPACK: /mnt/mfs/cluster/bin/R-3.4/lib/libRlapack.so
+
+locale:
+[1] C
+
+attached base packages:
+[1] stats     graphics  grDevices utils     datasets  methods   base
+
+other attached packages:
+[1] data.table_1.11.8 purrr_0.2.5       dplyr_0.7.7
+
+loaded via a namespace (and not attached):
+ [1] tidyselect_0.2.5 compiler_3.4.2   magrittr_1.5     assertthat_0.2.0
+ [5] R6_2.3.0         pillar_1.3.0     bindrcpp_0.2.2   glue_1.3.0
+ [9] tibble_1.4.2     crayon_1.3.4     Rcpp_0.12.19     pkgconfig_2.0.2
+[13] rlang_0.3.0.1    bindr_0.1.1
+"
+
+
+#SD_perprobeset<-as.data.frame(apply(exon_probsets_genefounds[,2:225],1,sd))
+#SD_perprobeset$probeset_id<-exon_probsets_genefounds$V1
+#colnames(SD_perprobeset)<-c("sd","V1")
+
+#sd_perprobeset_joined_annot.transcript<-left_join(SD_perprobeset,filtered_annotation_foundexonexpression,by=c("V1"="probeset_id"))
+#median_pertranscript_fromprobeset.sd<-as.data.frame(sd_perprobeset_joined_annot.transcript %>% group_by(transcript_cluster_id) %>% summarize_at(vars(sd),funs(median))  %>% 
+#rename(median_value=sd))  %>% 
+#left_join(.,sd_perprobeset_joined_annot.transcript,by=c("transcript_cluster_id"="transcript_cluster_id"))
+#
+#Normalized SI
+#
+#joined_SI_median_withintranscript<-left_join(exonexp.ratio_with_geneexpres,median_pertranscript_fromprobeset.sd,by=c("id"="V1"))
+###########
