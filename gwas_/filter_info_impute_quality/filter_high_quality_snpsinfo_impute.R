782e4d96ea6852e86d9c920cd509839ee80731c8
Sanjeev Sariya <sanjeevsariya@gmail.com>
2018-12-06 15:12:36 -0500

    initial code

--- gwas_/filter_info_impute_quality/filter_high_quality_snpsinfo_impute.R
+++ gwas_/filter_info_impute_quality/filter_high_quality_snpsinfo_impute.R
@@ -10,3 +10,23 @@
 #take quality
 #take info file
 #take .posterior probability
+
+library("argparse")
+library(dplyr)
+parser <- ArgumentParser(description="filter snps based on quality")
+
+parser$add_argument('-i',"--info",help="input info file",required=TRUE) #file - that has info information 
+parser$add_argument('-p',"--prob",help="probability file",required=TRUE) #file output from king
+parser$add_argument('-q',"--qual",help="quality value",required=TRUE) #quality threshold
+parser$add_argument('-x',"--outpre",help="output prefix",required=TRUE) # prefix for output
+
+args <- parser$parse_args() #make it a data structure
+
+file.info<-normalizePath(args$info)
+file.probs<-normalizePath(args$prob)
+quality<-args$qual
+outputprefix<-args$outpre
+
+
+
+
