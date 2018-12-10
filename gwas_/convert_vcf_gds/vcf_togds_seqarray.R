77d0a97ea44cf6da226d60462b24ebdf28b76651
Sanjeev Sariya <sanjeevsariya@gmail.com>
2018-12-10 14:36:41 -0500

    code update

--- gwas_/convert_vcf_gds/vcf_togds_seqarray.R
+++ gwas_/convert_vcf_gds/vcf_togds_seqarray.R
@@ -5,4 +5,26 @@
 #Sanjeev Sariya
 #convert .vcf file for annoted snps to GDS. This GDS format is based on seqArray 
 
-#
+#https://www.bioconductor.org/packages/devel/bioc/vignettes/SeqArray/inst/doc/SeqArrayTutorial.html
+
+library(SeqArray)
+library(Rcpp)
+library("argparse")
+
+parser <- ArgumentParser(description="filter snps based on quality info and imputed prob file")
+
+parser$add_argument('-v',"--vcf",help="input vcf file ",required=TRUE) #
+parser$add_argument('-c',"--chr",help="output prefix",required=TRUE) # chr
+parser$add_argument('-x',"--outpre",help="output prefix",required=TRUE) # prefix for output
+args <- parser$parse_args() #make it a data structure
+
+output_prefix<-args$outpre
+chr<-args$chr
+output_file<-paste(output_prefix,".gds",sep="")
+print("entering seqarray conversion")
+
+vcf.fn <- args$vcf
+
+seqVCF2GDS(vcf.fn, output_file, verbose=FALSE)
+
+print("exiting seqarray conversion")
