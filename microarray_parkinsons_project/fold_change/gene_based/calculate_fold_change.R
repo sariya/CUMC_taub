e437778753fab61c95491e6c02454f77b3969911
Sanjeev Sariya <sanjeevsariya@gmail.com>
2018-12-17 15:10:14 -0500

    calculate fold change per gene

---
+++ microarray_parkinsons_project/fold_change/gene_based/calculate_fold_change.R
@@ -0,0 +1,11 @@
+#!/bin/Rscript
+
+#
+#Date 12/17/2018
+#Sanjeev Sariya
+#Dr. Giuseppe Tosto, Karen Marder
+#
+######################################
+# Gene level normalized data
+######################################
+
