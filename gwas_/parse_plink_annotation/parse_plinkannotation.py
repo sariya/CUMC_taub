b77dfa1029e4ead9e2207ff774f31d7b866b96f3
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-01-16 11:55:32 -0500

    edits

--- gwas_/parse_plink_annotation/parse_plinkannotation.py
+++ gwas_/parse_plink_annotation/parse_plinkannotation.py
@@ -12,8 +12,7 @@
 #python parse_plinkannotation.py -i plink.annot -d ./ -p batch1_CHR1
 
 #
-" Python 2.7.13 (default, Jan 19 2017, 14:48:08)
-[GCC 6.3.0 20170118] on linux2 "
+" Python 2.7.13 (default, Jan 19 2017, 14:48:08) [GCC 6.3.0 20170118] on linux2 "
 import argparse,os
 
 if __name__=="__main__":
