de825b5a55cc67b26c91e278137771c91a98cc25
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-01-16 11:56:38 -0500

    edits for rfmix

--- gwas_/local_ancestry/rfmix_v2/merge_probability_per_ancestry.R
+++ gwas_/local_ancestry/rfmix_v2/merge_probability_per_ancestry.R
@@ -10,3 +10,12 @@
 #
 #
 #
+
+library("argparse")
+parser <- ArgumentParser(description="")
+parser$add_argument('-f',"--fb",help="input forward backward file",required=TRUE) #
+parser$add_argument('-c',"--chr",help="output prefix",required=TRUE) # chr
+parser$add_argument('-x',"--outpre",help="output prefix",required=TRUE) # prefix for output
+args <- parser$parse_args() #make it a data structure
+
+
