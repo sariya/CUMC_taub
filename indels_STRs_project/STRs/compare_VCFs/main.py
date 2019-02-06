d3ab7ebf9e760c8b69dbbe1a21de5b5b4ea0d770
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-02-06 14:22:41 -0500

    set positions

--- indels_STRs_project/STRs/compare_VCFs/main.py
+++ indels_STRs_project/STRs/compare_VCFs/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 #
 #Date 02-05-2019
 #Sanjeev Sariya
@@ -14,22 +13,40 @@
 #Takes two VFs - plink VCF from imputation and VCF from STR called.
 #
 
+#
+#Python 2.7.13 (default, Jan 19 2017, 14:48:08)
+#[GCC 6.3.0 20170118] on linux2
+#
+
 import vcf
+
+def get_pos(temp_vcffile):
+    
+    """
+    Read VCF file and send unique positions
+    """
+    
+    pos_store=set() #store and return later
+    vcf_reader_impute = vcf.Reader(filename=temp_vcffile)
+    
+    for record in vcf_reader_impute:
+        pos_store.add(record.POS)
+    #--for loop end
+
+    print len(pos_store)
+    return len(pos_store)
+    #---------------------
+    #func ends
+    #-----------------------    
 
 if __name__=="__main__":
 
-    imputed_vcf_file="/home/ss5505/scripts_cumc/indels_STRs_project/STRs/compare_VCFs/plink_updated_ids_STRs_68_0.8.vcf"    
-    called_file_vcf="/mnt/mfs/hgrcgrid/shared/GT_ADMIX/INDEL_comparisons/bam_files_processing_STR/compare_VCFs/normalized_CHR22_STR_fixedVersion.vcf.gz"
+    imputed_vcf_file="/mnt/mfs/hgrcgrid/shared/GT_ADMIX/INDEL_comparisons/bam_files_processing_STR/compare_VCFs/plink_updated_ids_STRs_68_0.8.vcf.gz"    
+    called_file_vcf="/mnt/mfs/hgrcgrid/shared/GT_ADMIX/INDEL_comparisons/bam_files_processing_STR/compare_VCFs/normalized_CHR22_STR_fixedVersion.vcf.gz"    
     
-    vcf_reader = vcf.Reader(filename=file_vcf)
+    sitepos_imputation=get_pos(imputed_vcf_file) #get site position in a set
+    sitepos_vcalled=get_pos(called_file_vcf)
 
-    for record in vcf_reader:
-        print record, record.start,record.end
-        print record.num_hom_ref, record.num_het, record.num_hom_alt       
-        break
-
-#    file_vcf="/mnt/mfs/hgrcgrid/shared/GT_ADMIX/INDEL_comparisons/bam_files_processing_STR/str_calls_38Fams_toolStutter.vcf.gz"
-#    vcf_reader = vcf.Reader(filename=file_vcf)
 
     print "Done reading"
 
