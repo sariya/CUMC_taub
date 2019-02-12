3b31fa0f6a75dc0edfdc75237baa2a84bba891d4
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-02-12 11:49:35 -0500

    headers and information

---
+++ gwas_/local_ancestry/rfmix_v1/prepare_mixscoreinput.R
@@ -0,0 +1,13 @@
+#!/bin/Rscript
+
+
+#Prepare data for MIXSCORE. Make Thrre-way admix
+#We're making CEU as our reference population. If two copies are found then 2 as value in matrix. If none than 0
+#We'are making NAT and YRI as one component. CEU against NAT+YRI . 
+#
+#
+#Sanjeev Sariya
+#PI Dr. Giuseppe Tosto
+#Date 02 FEB  2018
+#PH 19
+#
