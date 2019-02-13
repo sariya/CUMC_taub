2ab89c8692b2f8213985cd439d4ff3e4c2694690
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-02-13 12:41:28 -0500

    unique to STRs called and imputation

--- indels_STRs_project/STRs/compare_VCFs/main.py
+++ indels_STRs_project/STRs/compare_VCFs/main.py
@@ -43,6 +43,55 @@ def get_pos(temp_vcffile):
     #func ends
     #-----------------------    
 
+def find_only_calledsites(set_impute_sites, set_strcalled_sites):
+
+    print len(set_impute_sites)
+    print len(set_strcalled_sites)
+
+    """
+    We'll take sites from VCF and take sites - imputed and STR called. 
+    We'll find sites that are only present in STR called data
+    
+    """
+    
+    #work on element in impute but not in VCF called https://stackoverflow.com/a/50559206/2740831
+    sites_calledonly=(set_strcalled_sites^set_impute_sites)&set_strcalled_sites
+    print(len(sites_calledonly))
+
+    calledSTR_calledonly_list = list(sites_calledonly) #convert set into list to access elements # https://stackoverflow.com/a/32512277/2740831
+
+    print calledSTR_calledonly_list[1:10]
+    print "Leaving only called STRs function"
+    
+    #
+    #function ends
+    #<<->>
+    
+def find_only_imputationsites(set_impute_sites, set_strcalled_sites):
+
+    print len(set_impute_sites)
+    print len(set_strcalled_sites)
+
+    """
+    We'll take sites from VCF and take sites - imputed and STR called. 
+    We'll find sites that are only present in Imputed data
+    
+    """
+    
+    #work on element in impute but not in VCF called https://stackoverflow.com/a/50559206/2740831
+    sites_imputedonly=(set_impute_sites^set_strcalled_sites)&set_impute_sites
+    print(len(sites_imputedonly))
+
+    imputed_sitesonly_list = list(sites_imputedonly) #convert set into list to access elements # https://stackoverflow.com/a/32512277/2740831
+    #print sites_imputedonly[1:10]
+
+    print imputed_sitesonly_list[1:10]
+    print "Leaving only impute SNPs function"
+
+    #
+    #function ends
+    #<<->>
+    
 if __name__=="__main__":
 
     imputed_vcf_file="/mnt/mfs/hgrcgrid/shared/GT_ADMIX/INDEL_comparisons/IMPUTED_CHR22_batch2/plink_STRs_68_0.4.vcf"    
@@ -61,10 +110,12 @@ if __name__=="__main__":
     print len(my_overlapped_list)
     print my_overlapped_list[1:5]
 
+    #find sites unique to STR called and imputation
+
+    find_only_imputationsites(sitepos_imputation,sitepos_vcalled) # sent to find sites only imputation
+    find_only_calledsites(sitepos_imputation,sitepos_vcalled)
+
     #
-    #work on element in impute but not in VCF called https://stackoverflow.com/a/50559206/2740831
-    #
-    
     print "Done reading"
 
     
