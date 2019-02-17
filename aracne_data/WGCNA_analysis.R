a4e4fd89a2c558e192194f8331f578f67072fca3
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-02-17 16:55:52 -0500

    amanda myers sample and gene cleaning

--- aracne_data/WGCNA_analysis.R
+++ aracne_data/WGCNA_analysis.R
@@ -10,12 +10,13 @@
 #Tutorial at: #http://pklab.med.harvard.edu/scw2014/WGCNA.html##################
 #
 
+library(dplyr)
 library(WGCNA)
 library(Cairo)
 enableWGCNAThreads()
 options(stringsAsFactors = FALSE)
 
-exprs_data<-read.table("complete_genes_mapped",header=TRUE)
+exprs_data<-read.table("duplicate_genes_summed",header=TRUE)
 print(dim(exprs_data))
 
 #transpose the expression data
@@ -49,17 +50,19 @@ sampleTree = hclust(dist(data_exprs.cleaned ), method = "average");
 # Plot the sample tree: 
 # The user should change the dimensions if the window is too large or too small.
 
-
-CairoJPEG("sample_outliers_tree.jpeg",width=1200,height=900)
-par(cex = 0.6);
+#Not removing any samples from Amanda myers brain expression data
+#
+CairoJPEG("sample120_outliers_tree.jpeg",width=1200,height=900)
+par(cex = 0.8);
 par(mar = c(0,4,2,0))
 plot(sampleTree, main = "Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5,cex.axis = 1.5, cex.main = 2)
-abline(h=90, col = "red")
+abline(h=120, col = "red")
 dev.off()
 
 print("Printed JPEG file for sample tree to see outlier")
 
-labels_def = cutreeStatic(sampleTree, cutHeight = 95)  #--default cluster size is 50
+#-for amanda myers data. I'm using 140 as cut height and losing 29 samples out of 363. Less than 10% is acceptable
+labels_def = cutreeStatic(sampleTree, cutHeight = 140)  #--default cluster size is 50
 table(labels_def )
 keepSamples = (labels_def==1)
 
@@ -67,14 +70,14 @@ datExpr = data_exprs.cleaned [keepSamples, ]
 nGenes = ncol(data_exprs.cleaned )
 nSamples = nrow(data_exprs.cleaned )
 
-
+#datExpr  is new data expression frame
 ##################################################################
 #Automatic, one-step network construction and module detection:
 ##################################################################
 
-
+#datExpr  is new data expression frame
 powers = c(c(1:10), seq(from = 12, to=20, by=2))
-sft = pickSoftThreshold(data_exprs.cleaned, powerVector = powers, verbose = 5)
+sft = pickSoftThreshold(datExpr, powerVector = powers, verbose = 5) # work with rmoeved samples
 CairoJPEG("soft_gene.jpeg",height=900,width=1000)
 
 par(mfrow = c(1,2));
@@ -96,17 +99,19 @@ text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
 dev.off()
 #############################
 
-softPower = 8;
-adjacency = adjacency(data_exprs.cleaned, power = softPower);
+softPower = 6; #for amnda myers data we are using 6 as sft threshold power 334 samples
+adjacency = adjacency(datExpr, power = softPower);
 
 # Turn adjacency into topological overlap
 TOM = TOMsimilarity(adjacency);
 dissTOM = 1-TOM
 
+print(dim(dissTOM)) # -- should be sq matrix with genes
+
 # Call the hierarchical clustering function
 geneTree = hclust(as.dist(dissTOM), method = "average");
 # Plot the resulting clustering tree (dendrogram)
-CairoJPEG("result_clustering_gene.jpeg",height=900,width=1000)
+CairoJPEG("result_bnwclustering_gene_0.4.jpeg",height=900,width=1000)
 plot(geneTree, xlab="", sub="", main = "Gene clustering on TOM-based dissimilarity", labels = FALSE, hang = 0.04);
 dev.off()
 
@@ -118,6 +123,9 @@ dynamicMods = cutreeDynamic(dendro = geneTree, distM = dissTOM,
                 minClusterSize = minModuleSize);
 table(dynamicMods)
 
+#
+#21422 genes
+#
 # Convert numeric lables into colors
 dynamicColors = labels2colors(dynamicMods)
 table(dynamicColors)
@@ -128,8 +136,9 @@ dev.off()
 
 #discard the unassigned genes, and focus on the rest
 restGenes= (dynamicColors != "grey")
-diss1=1-TOMsimilarityFromExpr(data_exprs.cleaned[,restGenes], power = softPower)
+diss1=1-TOMsimilarityFromExpr(datExpr[,restGenes], power = softPower) #samples cleaned
 
+print(dim(diss1)) #19499 19499 we lose 1923 genes
 colnames(diss1) =rownames(diss1) =SubGeneNames[restGenes]
 hier1=hclust(as.dist(diss1), method="average" )
 CairoJPEG("module_colors_nogrey.jpeg",height=900,width=1000)
@@ -140,37 +149,42 @@ dev.off()
 #set the diagonal of the dissimilarity to NA 
 diag(diss1) = NA;
 
-
 #Visualize the Tom plot. Raise the dissimilarity matrix to the power of 4 to bring out the module structure
 CairoJPEG("module_colors_nogrey_heatmap.jpeg",height=900,width=1000)
-TOMplot(diss1, hier1, as.character(dynamicColors[restGenes]))
+TOMplot(diss1, hier1, as.character(dynamicColors[restGenes]),main="Module clustered")
 dev.off()
 
-
 ###Extract modules
-gene.names<-colnames(data_exprs.cleaned)
+
+length(which(dynamicMods == 0)) #0 means grey color 
+datExpr_cleaned_greycolor<-datExpr[,-c(which(dynamicMods == 0))]
+gene.names<-colnames(datExpr_cleaned_greycolor) 
+#gene.names<-colnames(datExpr)
 SubGeneNames<-gene.names
+
+
+dynamiccolors_nogrey<-dynamicColors[dynamicColors!="grey"]
 
 module_colors= setdiff(unique(dynamicColors), "grey")
 for (color in module_colors){
-    module=SubGeneNames[which(dynamicColors==color)]
+    module=SubGeneNames[which(dynamiccolors_nogrey==color)]
     write.table(module, paste("module_",color, ".txt",sep=""), sep="\t", row.names=FALSE, col.names=FALSE,quote=FALSE)
 }
 
+module.order <- unlist(tapply(1:ncol(datExpr_cleaned_greycolor),as.factor(dynamiccolors_nogrey),I)) #samples cleaned
 
-module.order <- unlist(tapply(1:ncol(data_exprs.cleaned),as.factor(dynamicColors),I))
-m<-t(t(data_exprs.cleaned[,module.order])/apply(data_exprs.cleaned[,module.order],2,max))
+m<-t(t(datExpr_cleaned_greycolor[,module.order])/apply(datExpr_cleaned_greycolor[,module.order],2,max)) #samples cleaned
 
 CairoJPEG("heatmap_expression_profiles.jpeg",height=900,width=1000)
 heatmap(t(m),zlim=c(0,1),col=gray.colors(100),Rowv=NA,Colv=NA,labRow=NA,scale="none",RowSideColors=dynamicColors[module.order])
 dev.off()
 
-
 ### get value per moduel for person
 ###Quantify module similarity by eigengene correlation. Eigengenes: Module representatives
 ####
 
-MEList = moduleEigengenes(data_exprs.cleaned, colors = dynamicColors)
+MEList = moduleEigengenes(datExpr_cleaned_greycolor, colors = dynamiccolors_nogrey)
+##MEList = moduleEigengenes(data_exprs.cleaned, colors = dynamicColors)
 MEs = MEList$eigengenes
 
 CairoJPEG("eigennetworks.jpeg",height=900,width=1000)
@@ -192,28 +206,43 @@ dev.off()
 
 
 # Call an automatic merging function
-merge = mergeCloseModules(data_exprs.cleaned, dynamicColors, cutHeight = 0.30, verbose = 3)
+merge = mergeCloseModules(datExpr_cleaned_greycolor, dynamiccolors_nogrey, cutHeight = 0.30, verbose = 3) #we have 40 gene modules
+length(merge$colors) #19499 - after excluding grey color
+
+##merge = mergeCloseModules(data_exprs.cleaned, dynamicColors, cutHeight = 0.30, verbose = 3)
 # The merged module colors
 mergedColors = merge$colors;
 # Eigengenes of the new merged modules:
 mergedMEs = merge$newMEs
 
-
 #read disease status
 diseasestatus<-read.table("disease_status_sample",header=TRUE)
-sample_cleaned_expression<-rownames(data_exprs.cleaned) ## get sample names
-sample_cleaned_expression==diseasestatus$Sample # check if they match
+sample_cleaned_expression<-rownames(datExpr_cleaned_greycolor) ## get sample names
+
+#merge them
+merge_samples_diseasestatus<-(merge(rownames(datExpr_cleaned_greycolor),diseasestatus,by=0)) 
+
+#make row.names as numeric 
+merge_samples_diseasestatus$Row.names <- as.numeric(merge_samples_diseasestatus$Row.names)
+
+#sort based on 
+merge_samples_diseasestatus<-(merge_samples_diseasestatus[order(merge_samples_diseasestatus$Row.names),] )
+ 
 
 ###Run logistic regression to check significance of any module
 
 df_colors_pvalue <- data.frame(matrix(ncol = 2, nrow =length(colnames(mergedMEs) )))
 colnames(df_colors_pvalue)<-c("colorModule","Pvalue")
 
+
+#
+#mergedMEs it has column count as colors and row count as samples 
+#
 for (i in 1:length(colnames(mergedMEs))){
 ##=- loop over different module's eigen values
 
 #--run linear logistic regression
-temp.pvalue<-(summary(lm(formula = diseasestatus$Status ~ mergedMEs[,i], data = MEs, family = "binomial"))$coeff)[2,4]
+temp.pvalue<-(summary(lm(formula = merge_samples_diseasestatus$Status ~ mergedMEs[,i], data = MEs, family = "binomial"))$coeff)[2,4]
 df_colors_pvalue[i,1] <-colnames(mergedMEs)[i]
 df_colors_pvalue[i,2] <-temp.pvalue
 
@@ -225,7 +254,7 @@ df_colors_pvalue<- df_colors_pvalue[order(df_colors_pvalue$Pvalue),]
 write.table(df_colors_pvalue,"colors_pvalue_regression",sep = "\t",quote=FALSE,col.names = TRUE,row.names = FALSE)
 
 #--use manual's method
-significance_module<-t(as.data.frame(signif(cor(diseasestatus$Status,mergedMEs, use="p"),2)))
+significance_module<-t(as.data.frame(signif(cor(merge_samples_diseasestatus$Status,mergedMEs, use="p"),2)))
 
 significance_module<- significance_module[order(significance_module[,1]),] 
 significance_module<-as.data.frame(significance_module)
@@ -243,12 +272,10 @@ GeneSignificance=abs(GS1)
 # Next module significance is defined as average gene significance.
 
 ModuleSignificance=tapply(GeneSignificance, merge$colors, mean, na.rm=T)
-
 ModuleSignificance<-(as.data.frame(ModuleSignificance))
 ModuleSignificance$color<-row.names(ModuleSignificance)
 
 ModuleSignificance<- ModuleSignificance[,c(2,1)]
-
 
 CairoJPEG("merged_colors_threshold.jpeg",height=900,width=1000,quality=75)
 plotDendroAndColors(geneTree, cbind(dynamicColors, mergedColors), c("Dynamic Tree Cut", "Merged dynamic"), dendroLabels = FALSE, hang = 0.03, addGuide = TRUE, guideHang = 0.05)
@@ -264,7 +291,6 @@ moduleColors = mergedColors
 colorOrder = c("grey", standardColors(50));
 moduleLabels = match(moduleColors, colorOrder)-1;
 MEs = mergedMEs;
-
 
 ##################################################################
 ## one step network construction
