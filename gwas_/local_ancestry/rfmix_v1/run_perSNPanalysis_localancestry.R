d6ad5e4e9d894d70ae11f9ad4c3b64f1819d4e76
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-02-19 14:07:47 -0500

    Information

--- gwas_/local_ancestry/rfmix_v1/run_perSNPanalysis_localancestry.R
+++ gwas_/local_ancestry/rfmix_v1/run_perSNPanalysis_localancestry.R
@@ -1,6 +1,13 @@
 #!/bin/Rscript
 
 #Date 02/19/2019
+#Sanjeev Sariya 
+#PI - Giuseppe Tosto
 #use output
 #
+#
+
+#Date - input - per chromsome . SNP file with names and derived allele per person per snp.
+#Local ancestry is home-made from RFmix viterbi file. 
+#Total columns would be N_people multipleid by 3. Three - CEU/YRI/NAT
 #
