6bb670dd04cfdb7facdf3cc42c6182ba46de8a3a
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-02-21 17:09:30 -0500

    initial lines and code

--- gwas_/local_ancestry/rfmix_v1/calculateswitchperperson.R
+++ gwas_/local_ancestry/rfmix_v1/calculateswitchperperson.R
@@ -3,4 +3,19 @@
 #
 #Date 02/21/2019
 #Sanjeev Sariya 
+#Take in coded RFmix output and calculate per person switch
+
 #
+#Column are persons and rows are SNPs #take in Viterbi output
+library(data.table)
+library(argpasre)
+
+
+rfmix_coded.file<-"ll"
+
+df.rfmixcoded<-data.table::fread(rsisd.file, showProgress = TRUE,header=FALSE)
+df.dosage<-data.table::fread("test", showProgress = TRUE,header=FALSE,sep=",")
+print(dim(df.dosage))
+
+
+
