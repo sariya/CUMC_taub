d7e46e03f76629651ce83e68b28c6e54bb2c9331
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-04-22 18:22:53 -0400

    code jointing and CHR POS

--- gwas_/local_ancestry/admixMapMM/perform_admixMapMM.R
+++ gwas_/local_ancestry/admixMapMM/perform_admixMapMM.R
@@ -23,6 +23,7 @@ file.snps<-"/mnt/mfs/hgrcgrid/shared/GT_ADMIX/CHGWAS_analyses_data/multi_merged_
 #Read in files and get their dimensions
 #
 
+chr<-22
 df.snps<-read.csv(file.snps,header=FALSE)
 df.afr<-read.csv(file.afr,header=FALSE)
 df.nat<-read.csv(file.nat,header=FALSE)
@@ -33,6 +34,7 @@ print(dim(df.nat))
 print(dim(df.ceu))
 print(dim(df.snps))
 
+colnames(df.snps)<-c("rsids","a1","a2")
 #
 #Check nrow in order to ensure data look good.
 #		
@@ -47,5 +49,51 @@ stop("issue with SNPs nrow")
 #
 #Check and reading ends
 #
+
+#
+#Read file with rs ids and chr:pos
+#
+
+file.rsPos<-"/mnt/mfs/hgrcgrid/shared/GT_ADMIX/CHGWAS_analyses_data/multi_merged_data/merge_MESA_NOMAS_HGWAS126/local_ancestry_analysis/localancestry_analysis/RFMIX_datapreparations/ancestry_components/rsids_allchrs_positions"
+
+df.rsPos<-read.table(file.rsPos,header=FALSE)
+print(dim(df.rsPos))
+df.rsPos$V1<-as.character(df.rsPos$V1)
+df.rsPos$V2<-as.character(df.rsPos$V2)
+
+temp<-strsplit(df.rsPos$V1,"\\:") #split column for RS
+rs_chrmatrix <- do.call(rbind,temp) #do binding
+rs_chrmatrix <- as.data.frame(rs_chrmatrix,stringsAsFactors = FALSE)
+rs_chrmatrix$rsids<- df.rsPos$V2
+rs_chrmatrix$V1<-as.numeric(as.character(rs_chrmatrix$V1))
+rs_chrmatrix$V2<-as.numeric(as.character(rs_chrmatrix$V2))
+
+#
+#get length of positions and RSids that match given CHRs
+#
+print(length(which(rs_chrmatrix$V1==chr)))
+
+rs_chrmatrix<-rs_chrmatrix[which(rs_chrmatrix$V1==chr),]
+
+colnames(rs_chrmatrix)<-c("CHR","POS","rsids")
+jointed_rschrpos<-left_join(df.snps,rs_chrmatrix,by=c("rsids"))
+
+if(sum(!complete.cases(jointed_rschrpos)) !=0){
+
+stop("we have some issue jhere with Rsids and positions sum not equal to zero!!!!!")
+}
+
+
+if(nrow(jointed_rschrpos[complete.cases(jointed_rschrpos),]) != nrow(df.ceu)){
+stop("we have issue when taking complete cases and nrow with CEU")
+}
+
+#
+#First three columns are useless in NAT/CEU/AFR 
+#
+
+df.afr<-df.afr[,-c()]
+df.ceu<-df.ceu[,]
+df.nat<-df.nat[,]
 print("Ending Script")
 
