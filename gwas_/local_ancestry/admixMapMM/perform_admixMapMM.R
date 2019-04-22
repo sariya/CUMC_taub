4c029f19a9b045af2d588e775884b064b6d07534
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-04-22 17:46:09 -0400

    admixmapping R code

---
+++ gwas_/local_ancestry/admixMapMM/perform_admixMapMM.R
@@ -0,0 +1,14 @@
+#!/usr/bin/Rscript
+
+#
+#Date 04/22/2019
+#Sanjeev Sariya
+#Local Ancestry
+#https://rdrr.io/bioc/GENESIS/man/admixMapMM.html
+
+print("Begin Script")
+
+file.afr<-
+file.nat<-
+file.ceu<-
+print("Ending Script")
