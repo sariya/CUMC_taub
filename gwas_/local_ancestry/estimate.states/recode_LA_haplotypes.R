dd087456ce14e134f93225cbaeda92860370585e
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-05-14 17:03:52 -0400

    code for states indented

--- gwas_/local_ancestry/estimate.states/recode_LA_haplotypes.R
+++ gwas_/local_ancestry/estimate.states/recode_LA_haplotypes.R
@@ -10,12 +10,10 @@
 # 
 library("argparse")
 
-#
 #RFmix 1.5.X
 #The output has admixed individuals columns. Per person one colum
 #1- is NAT, 2 is CEU and 3- is Yurobian as the information in output  #--it depends how your input in class file is designed.  
 
-##
 
 parser <- ArgumentParser(description="make ancetry from haps in rfmix v1 output")
 parser$add_argument('-v',"--vit",help="input viterbifile",required=TRUE) #
@@ -51,59 +49,59 @@ ancestry_mat<-matrix(0,nrow = 1, ncol = 1)
 if(x==y){
     if(x==1){
         ancestry_mat[1,1]<-1
-}
-#1- is NAT, 2 is CEU and 3- is Yurobian
-if(x==2){
-    ancestry_mat[1,1]<-2
-}
-#1- is NAT, 2 is CEU and 3- is Yurobian
-if(x==3){
-    ancestry_mat[1,1]<-3
-}
-#1- is NAT, 2 is CEU and 3- is Yurobian
+    }
+##1- is NAT, 2 is CEU and 3- is Yurobian
+    if(x==2){
+        ancestry_mat[1,1]<-2
+    }
+###1- is NAT, 2 is CEU and 3- is Yurobian
+    if(x==3){
+        ancestry_mat[1,1]<-3
+    }
+##1- is NAT, 2 is CEU and 3- is Yurobian
 } else{ 
 
-#--if 100% not from one ancestry
+##--if 100% not from one ancestry
 
-if((x==1 & y==2) |(x==2 & y==1) ){
-    ancestry_mat[1,1]<-4
-}
-if ((y==2 & x==3) | (x==2 & y==3)){
-    ancestry_mat[1,1]<-5
-}
+    if((x==1 & y==2) |(x==2 & y==1) ){
+        ancestry_mat[1,1]<-4
+    }
+    if ((y==2 & x==3) | (x==2 & y==3)){
+        ancestry_mat[1,1]<-5
+    }
 
-#1- is NAT, 2 is CEU and 3- is Yurobian
-if((x==1 & y==3) | ( x==3 & y==1 )){
-    ancestry_mat[1,1]<-6
-}
+###1- is NAT, 2 is CEU and 3- is Yurobian
+    if((x==1 & y==3) | ( x==3 & y==1 )){
+        ancestry_mat[1,1]<-6
+    }
 
 } ##else ends 
 
-return(ancestry_mat)
+    return(ancestry_mat)
 }
 #############################################
 #get_ancestry function ends
 #############################################
 
-
 haploStates_ancestry_per_person<-apply(df.viterbi,1,function(i)
 {
 
 #--make data frame of row
-row_df<-t(as.data.frame(i))
-
-iterate<-ncol(row_df)/2 #get variable to iterate over columns in a row
-
-#--iterate over columns
-ancestral_components<-sapply(1:iterate,function(index){
+    row_df<-t(as.data.frame(i))
+    
+    iterate<-ncol(row_df)/2 #get variable to iterate over columns in a row
+    
+    ##--iterate over columns
+    ancestral_components<-sapply(1:iterate,function(index){
 	
-    t<-index*2 #t is used for index in a vector
-	
-    output_mat<-get_ancestry(row_df[t-1],row_df[t])
-    return(output_mat)
+        t<-index*2 #t is used for index in a vector
+        output_mat<-get_ancestry(row_df[t-1],row_df[t]) ##we're accessing data using index.
+        return(output_mat)
+    })
+    ##sapply ends
+    
 })
-      
-})
+###apply ends
 
 print(dim(haploStates_ancestry_per_person))
 print(dim(t(haploStates_ancestry_per_person)))
@@ -112,6 +110,8 @@ outfile<-paste(prefix,".txt",sep="")
 write.table(t(haploStates_ancestry_per_person), file = outfile, append = FALSE, quote = FALSE, sep = "\t",row.names = FALSE,col.names = FALSE) #write to an output file
 
 print("check current working directory")
+
+
 
 
 
