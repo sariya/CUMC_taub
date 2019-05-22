761e0f39652910e81def7502f04d6a26b12c7983
Sanjeev Sariya <sanjeevsariya@gmail.com>
2019-05-22 13:28:49 -0400

    code to merge repeat counts with VCF information per person

---
+++ indels_STRs_project/STRs/WGS_ROSMAP/get_repeatcount/merge.repeat_countsVCF.R
@@ -0,0 +1,16 @@
+#!/bin/Rscript
+
+#
+#Date 05/22/2019
+#Sanjeev Sariya
+##badri, Giuseppe 
+
+##/usr/local/bin/Rscript
+#
+#Read in VCF
+#Create genotype based on repeat count and allele information 
+#from VCF GT : 0|0, 0|1, so on and so forth
+#
+
+library(vcfR)
+#library(argparse)
