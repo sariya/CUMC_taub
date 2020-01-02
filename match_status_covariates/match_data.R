2e74ab7ab5ff260b63da10f1674ac4b4f2fddd93
Sanjeev Sariya <sanjeevsariya@gmail.com>
2020-01-02 14:41:24 -0500

    edited indentation

--- match_status_covariates/match_data.R
+++ match_status_covariates/match_data.R
@@ -5,30 +5,30 @@
 #Sanjeev Sariya
 #
 
+##function to check check columns. 
 check_columns<-function(cols1,cols2){
 
-print("we are here")
-if(length(cols1)!=length(cols2)){
-stop("column number isn't same. Exiting")
+    if(length(cols1)!=length(cols2)){
+        ##column number has to be same in both data frames
+        stop("column number isn't same. Exiting")
 
-}
-##if ends to check column number
+    }
+    ##if ends to check column number
 
-if(length(match(colnames(df2),colnames(df1))) != length(colnames(df2))){
-stop("columns don't match. Exiting")
+    if(length(match(colnames(df2),colnames(df1))) != length(colnames(df2))){
+        ##column names must match
+        stop("columns don't match. Exiting")
 
-}
-##first match ends 
+    }
+    ##first match ends 
 
-if(length(match(colnames(df1),colnames(df2))) != length(colnames(df1))){
-stop("columns don't match. Exiting")
+    if(length(match(colnames(df1),colnames(df2))) != length(colnames(df1))){
+        stop("columns don't match. Exiting")
 
-}
+    }
 ##second match ends 
 
-
 }
-
 #function check_columns ends 
 
 file1<-"input_one.txt"
