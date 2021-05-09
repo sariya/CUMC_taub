ef1b668ef69813d388fb0c08ef20704322ec5138
Sanjeev Sariya <sanjeevsariya@gmail.com>
2021-05-09 18:38:46 -0400

    changes

--- c_code/parse_annovar_annotation/main_parse_annotation.c
+++ c_code/parse_annovar_annotation/main_parse_annotation.c
@@ -7,27 +7,22 @@
 #include "parse_function.h"
 
 //////////////////
-//////////////////
-//////////////////
-
-//////////////////
-
-//////////////////////////////
-//////////////////////////////
-
 
 int main(int argc, char *argv[])
 {
     // gcc -Wpedantic -Wextra -Wall *.c -o ./parse_annot
 
     gene_store *start_gene = NULL;
+    //
+    //   FILE *fp = fopen("/mnt/mfs/hgrcgrid/shared/MHAS/data/GENOTYPED/ANALYSES/gene_basedanalysis/annotation/INFO40/CHR22_MHAS_INFO40_plink.hg38_multianno.txt", "r");
 
-    FILE *fp = fopen("small_input.txt", "r");
+    FILE *fp = fopen("test_commas.txt", "r");
     if (fp == NULL)
         exit(EXIT_FAILURE);
 
     char *line = NULL;
     size_t len = 0;
+    int line_number = 0;
     while ((getline(&line, &len, fp)) != -1)
     {
         remove_trailingspaces(line);
@@ -35,61 +30,63 @@ int main(int argc, char *argv[])
         char SNP[400];
         token = strtok(line, "\t");
         int count_split = 1;
-        while (token != NULL && ++count_split <= 8)
+
+        if (line_number > 1)
         {
-            if (count_split == 2)
+            while (token != NULL && ++count_split <= 8)
             {
-                chr_concat(token, SNP); //make 21 as chr21:
-            }
-            if (count_split == 3)
-            {
-                join_strings(token, ':', SNP); //make  chr21 as chr21:1234:
-            }
-            if (count_split == 5)
-            {
-                join_strings(token, ':', SNP); //make   chr21:1234: as chr21:1234:A:
-            }
-            if (count_split == 6)
-            {
-                join_strings(token, '\0', SNP); //make  chr21:1234:A as chr21:1234:A:T
-            }
 
-            if (count_split == 8)
-            {
-                /**
-                 * if column eigth
-                */
-                if (check_comma(token) == 1)
+                if (count_split == 2)
                 {
-                    /**
-                     * If there is comma in gene columns
-                    */
+                    chr_concat(token, SNP); //make 21 as chr21:
+                }
+                if (count_split == 3)
+                {
+                    join_strings(token, ':', SNP); //make  chr21 as chr21:1234:
+                }
+                if (count_split == 5)
+                {
+                    join_strings(token, ':', SNP); //make   chr21:1234: as chr21:1234:A:
+                }
+                if (count_split == 6)
+                {
+                    join_strings(token, '\0', SNP); //make  chr21:1234:A as chr21:1234:A:T
+                }
 
-                    //printf("we have SNP as %s\n", SNP);
+                if (count_split == 8)
+                {
+
                     char *gene_token = NULL;
-                    gene_token = strtok(token, ",");
+                    const char *delimiters = ",;";
+                    //gene_token = strtok(token, ",;");
+                    gene_token = strtok(token, delimiters);
 
                     while (gene_token)
                     {
-                        if(search_gene(&start_gene, gene_token) ==0){
-
+                        printf("we have gene name as %s\n", gene_token);
+                        if (search_gene(&start_gene, gene_token) == 0)
+                        {
                             gene_add_node(&start_gene, gene_token, SNP);
                         }
-                       // printf("value of gene %s\n", gene_token); //printing each token
-                        gene_token = strtok(NULL, "\t");
+                        else
+                        {
+                            add_SNP_to_exiting_gene(&start_gene, gene_token, SNP);
+                        }
+                        //gene_token = strtok(NULL, ",;");
+
+                        gene_token = strtok(NULL, delimiters);
                     }
                 }
-                else
-                {
-                    printf("No  comma in the column\n", token);
-                }
-            }
-            //check for column 8th - that is where all genes are stored.
+                //check for column 8th - that is where all genes are stored.
 
-            token = strtok(NULL, "\t");
+                token = strtok(NULL, "\t");
+            }
+            SNP[0] = '\0';
+            // using printf() in all tests for consistency
+            //printf("line is %s\n", line);
         }
-        // using printf() in all tests for consistency
-        //printf("line is %s\n", line);
+        // printf("we have line number as %d\n", line_number);
+        line_number++;
     }
     fclose(fp);
 
@@ -97,7 +94,7 @@ int main(int argc, char *argv[])
     {
         free(line);
     }
-    display_gene_list(start_gene);
+    //    display_gene_list(start_gene);
     delete_linked_list_gene(&start_gene);
 
     return 0;
