c9e3249879ce3351f319361003de749c224c9147
Sanjeev Sariya <sanjeevsariya@gmail.com>
2021-08-03 13:55:28 -0400

    GMMAT group: c++ added changes to parse for strings and deleted old funcs

--- c_code/create_group_files_GMMAT/functions_groups_gmmat.cpp
+++ c_code/create_group_files_GMMAT/functions_groups_gmmat.cpp
@@ -3,8 +3,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "functions_groups_gmmat.h"
+#include "functions_groups_gmmat.hpp"
+void print_usage()
+{
+    printf("Usage: ./create_group_gmmat -a annovar_file -o output_file\n");
+}
+///
 
+void assign_variables(int count_argc, char **argv_array, const char *short_array, const struct option long_opt_temp[], char **temp_annovar_file, char **temp_out_file)
+{
+    /**
+     * We need a function to assign variables
+    */
+
+    int opt;
+    while ((opt = getopt_long(count_argc, argv_array, short_array, long_opt_temp, NULL)) != -1)
+    {
+        switch (opt)
+        {
+        case 'a':
+            *temp_annovar_file = optarg;
+            break;
+
+        case 'o':
+            *temp_out_file = optarg;
+            break;
+        case 'h':
+            print_usage();
+            exit(0);
+            //break;
+        }
+        //switch case ends
+    }
+}
+////////
 void remove_trailingspaces(char *newline)
 {
     /**
@@ -22,20 +54,20 @@ void remove_trailingspaces(char *newline)
         newline[line_length - 1] = '\0';
     }
 }
-///
-char *mystrcat(char *dest, char *src)
-{
-    /** function to concatenate two strings. 
-     * https://stackoverflow.com/a/21881314/2740831
-    */
-    while (*dest)
-    {
-        dest++;
-    }
-    while (*dest++ = *src++)
-    {
-        ;
-    }
+// ///
+// char *mystrcat(char *dest, char *src)
+// {
+//     /** function to concatenate two strings. 
+//      * https://stackoverflow.com/a/21881314/2740831
+//     */
+//     while (*dest)
+//     {
+//         dest++;
+//     }
+//     while (*dest++ = *src++)
+//     {
+//         ;
+//     }
 
-    return --dest;
-}
+//     return --dest;
+// }
--- c_code/create_group_files_GMMAT/functions_groups_gmmat.hpp
+++ c_code/create_group_files_GMMAT/functions_groups_gmmat.hpp
@@ -1,7 +1,12 @@
 #ifndef FUNC_create_group_H
 #define FUNC_create_group_H
+using namespace std;
+#include <string>
+#include <getopt.h>
+void print_usage();
+void assign_variables(int count_argc, char **argv_array, const char *short_array, const struct option long_opt_temp[], char **temp_annovar_file, char **temp_out_file);
 
 void remove_trailingspaces(char *newline);
-char *mystrcat(char *dest, char *src) ;
+//char *mystrcat(char *dest, char *src) ;
 #endif
 
